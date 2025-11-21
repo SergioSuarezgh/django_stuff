0ca44a71d4218a0c67297519690e0ca2b3f02ba0
Sergio <sersuach@gmail.com>
2025-11-21 18:02:01 +0100

    templates y creacion de pagina home

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,3 +1,10 @@
-setTimeout(function(){
-    $('#messages').fadeOut('slow')
-}, 5000)
+setTimeout(() => {
+    const el = document.getElementById("messages");
+    if (el) {
+        el.style.transition = "opacity 0.6s";
+        el.style.opacity = "0";
+
+        // Opcional: quitarlo del flujo después del fadeOut
+        setTimeout(() => el.style.display = "none", 600);
+    }
+}, 5000);
